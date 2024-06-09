71f230c1c2a223eb2125a102195f9cc4a105cb01
unknown <devken4u@gmail.com>
2024-06-08 17:11:10 -0800

    updated the ui and adds some logic

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,33 +1,131 @@
 import "./calculator.css";
+import { useState, useReducer } from "react";
 
-function CalculatorButton({ className, value }) {
-  return <div className={className}>{value}</div>;
+function CalculatorButton({ className, value, buttonClick, type }) {
+  return (
+    <div className={className} onClick={() => buttonClick(value, type)}>
+      {value}
+    </div>
+  );
 }
 
 function Calculator() {
+  // const [equation, setEquation] = useState("");
+  const [equation, dispatch] = useReducer(reducer, "");
+
+  function reducer(equation, action) {
+    switch (action.type) {
+      case "number":
+        if (equation == "" && action.value == "0") {
+          return "";
+        } else {
+          return equation + action.value;
+        }
+
+      case "decimal":
+        if (equation === "") {
+          return "0.";
+        } else if (equation.includes(".")) {
+          return equation;
+        } else {
+          return equation + ".";
+        }
+
+      case "clear":
+        return "";
+    }
+  }
+
+  function buttonClick(value, type) {
+    dispatch({ type: type, value: value });
+  }
+
   return (
     <div className="body-container">
       <div className="calculator-container">
-        <div className="calculator-screen">1</div>
+        <div className="calculator-screen">
+          <div className="equation">10 + 10 =</div>
+          <div className="current-value">{equation ? equation : "0"}</div>
+        </div>
         <div className="calculator-buttons">
-          <CalculatorButton className="button" value="C" />
+          <CalculatorButton
+            className="button"
+            value="C"
+            buttonClick={buttonClick}
+            type="clear"
+          />
           <CalculatorButton className="button" value="+/-" />
           <CalculatorButton className="button" value="%" />
-          <CalculatorButton className="button" value="&#247;" />
-          <CalculatorButton className="button" value="7" />
-          <CalculatorButton className="button" value="8" />
-          <CalculatorButton className="button" value="9" />
+          <CalculatorButton className="button" value="/" />
+          <CalculatorButton
+            className="button"
+            value="7"
+            buttonClick={buttonClick}
+            type="number"
+          />
+          <CalculatorButton
+            className="button"
+            value="8"
+            buttonClick={buttonClick}
+            type="number"
+          />
+          <CalculatorButton
+            className="button"
+            value="9"
+            buttonClick={buttonClick}
+            type="number"
+          />
           <CalculatorButton className="button" value="x" />
-          <CalculatorButton className="button" value="4" />
-          <CalculatorButton className="button" value="5" />
-          <CalculatorButton className="button" value="6" />
+          <CalculatorButton
+            className="button"
+            value="4"
+            buttonClick={buttonClick}
+            type="number"
+          />
+          <CalculatorButton
+            className="button"
+            value="5"
+            buttonClick={buttonClick}
+            type="number"
+          />
+          <CalculatorButton
+            className="button"
+            value="6"
+            buttonClick={buttonClick}
+            type="number"
+          />
           <CalculatorButton className="button" value="-" />
-          <CalculatorButton className="button" value="1" />
-          <CalculatorButton className="button" value="2" />
-          <CalculatorButton className="button" value="3" />
+          <CalculatorButton
+            className="button"
+            value="1"
+            buttonClick={buttonClick}
+            type="number"
+          />
+          <CalculatorButton
+            className="button"
+            value="2"
+            buttonClick={buttonClick}
+            type="number"
+          />
+          <CalculatorButton
+            className="button"
+            value="3"
+            buttonClick={buttonClick}
+            type="number"
+          />
           <CalculatorButton className="button" value="+" />
-          <CalculatorButton className="button zero" value="0" />
-          <CalculatorButton className="button" value="." />
+          <CalculatorButton
+            className="button zero"
+            value="0"
+            buttonClick={buttonClick}
+            type="number"
+          />
+          <CalculatorButton
+            className="button"
+            value="."
+            buttonClick={buttonClick}
+            type="decimal"
+          />
           <CalculatorButton className="button" value="=" />
         </div>
       </div>
