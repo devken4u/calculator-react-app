c92041f4b80149440ed985081cba3baa737d3ec6
unknown <devken4u@gmail.com>
2024-06-09 13:22:25 -0800

    added features

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,5 +1,5 @@
 import "./calculator.css";
-import { useReducer } from "react";
+import { useRef, useState } from "react";
 
 function CalculatorButton({ className, value, buttonClick, type }) {
   return (
@@ -9,45 +9,120 @@ function CalculatorButton({ className, value, buttonClick, type }) {
   );
 }
 
+function ThemePicker() {
+  const themeContainer = useRef("");
+  const theme1 = getTheme("--theme-1");
+  const theme2 = getTheme("--theme-2");
+  const theme3 = getTheme("--theme-3");
+
+  function showThemes() {
+    themeContainer.current.classList.toggle("hide");
+  }
+
+  function changeTheme(color, event) {
+    document.documentElement.style.setProperty(
+      "--calculator-background",
+      color
+    );
+    // event.stopPropagation();
+  }
+
+  function getTheme(themeName) {
+    const rootStyles = getComputedStyle(document.documentElement);
+    const bgColor = rootStyles.getPropertyValue(themeName).trim();
+    return bgColor;
+  }
+
+  return (
+    <div className="theme-picker-container" onClick={showThemes}>
+      <div className="theme-wrapper hide" ref={themeContainer}>
+        <div className="theme-container">
+          <div
+            className="theme theme-1"
+            onClick={(event) => changeTheme(theme1, event)}
+          ></div>
+          <div
+            className="theme theme-2"
+            onClick={(event) => changeTheme(theme2, event)}
+          ></div>
+          <div
+            className="theme theme-3"
+            onClick={(event) => changeTheme(theme3, event)}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 function Calculator() {
-  const [equation, dispatch] = useReducer(reducer, "");
+  const [equation, setEquation] = useState("");
+  const [preview, setPreview] = useState("");
 
-  function reducer(equation, action) {
+  function buttonClick(value, type) {
     // eslint-disable-next-line
-    switch (action.type) {
+    switch (type) {
       case "number":
-        if (equation === "" && action.value === "0") {
-          return "";
+        if (value === "0" && equation === "") {
+          setEquation("");
         } else {
-          return equation + action.value;
+          setEquation(equation + value);
         }
-
-      case "decimal":
-        if (equation === "") {
-          return "0.";
-        } else if (equation.includes(".")) {
-          return equation;
-        } else {
-          return equation + ".";
-        }
-
-      case "operator":
         break;
 
       case "clear":
-        return "";
-    }
-  }
+        setEquation("");
+        setPreview("");
+        break;
 
-  function buttonClick(value, type) {
-    dispatch({ type: type, value: value });
+      case "decimal":
+        if (equation === "") {
+          setEquation("0.");
+        } else if (!equation.includes(".")) {
+          setEquation(equation + ".");
+        }
+        break;
+
+      case "operator":
+        if (!(equation === "")) {
+          setPreview(equation + " " + value);
+          setEquation("");
+        }
+        break;
+
+      case "equals":
+        if (!(preview === "" && equation === "")) {
+          // eslint-disable-next-line
+          const result = eval(preview + equation);
+          setPreview("");
+          setEquation(result);
+        }
+        break;
+
+      case "sign":
+        if (equation.includes("-")) {
+          setEquation(equation.slice(1));
+        } else if (equation !== "") {
+          setEquation("-" + equation);
+        }
+        break;
+
+      case "percent":
+        if (!(equation === "")) {
+          // eslint-disable-next-line
+          const result = eval(equation / 100);
+          setEquation(result);
+        }
+        break;
+    }
   }
 
   return (
     <div className="body-container">
+      <ThemePicker />
       <div className="calculator-container">
         <div className="calculator-screen">
-          <div className="equation">10 + 10 =</div>
+          <div className="equation">{preview}</div>
           <div className="current-value">{equation ? equation : "0"}</div>
         </div>
         <div className="calculator-buttons">
@@ -57,9 +132,24 @@ function Calculator() {
             buttonClick={buttonClick}
             type="clear"
           />
-          <CalculatorButton className="button" value="+/-" />
-          <CalculatorButton className="button" value="%" />
-          <CalculatorButton className="button" value="/" />
+          <CalculatorButton
+            className="button"
+            value="+/-"
+            buttonClick={buttonClick}
+            type="sign"
+          />
+          <CalculatorButton
+            className="button"
+            value="%"
+            buttonClick={buttonClick}
+            type="percent"
+          />
+          <CalculatorButton
+            className="button"
+            value="/"
+            buttonClick={buttonClick}
+            type="operator"
+          />
           <CalculatorButton
             className="button"
             value="7"
@@ -78,7 +168,12 @@ function Calculator() {
             buttonClick={buttonClick}
             type="number"
           />
-          <CalculatorButton className="button" value="x" />
+          <CalculatorButton
+            className="button"
+            value="*"
+            buttonClick={buttonClick}
+            type="operator"
+          />
           <CalculatorButton
             className="button"
             value="4"
@@ -97,7 +192,12 @@ function Calculator() {
             buttonClick={buttonClick}
             type="number"
           />
-          <CalculatorButton className="button" value="-" />
+          <CalculatorButton
+            className="button"
+            value="-"
+            buttonClick={buttonClick}
+            type="operator"
+          />
           <CalculatorButton
             className="button"
             value="1"
@@ -116,7 +216,12 @@ function Calculator() {
             buttonClick={buttonClick}
             type="number"
           />
-          <CalculatorButton className="button" value="+" />
+          <CalculatorButton
+            className="button"
+            value="+"
+            buttonClick={buttonClick}
+            type="operator"
+          />
           <CalculatorButton
             className="button zero"
             value="0"
@@ -129,7 +234,12 @@ function Calculator() {
             buttonClick={buttonClick}
             type="decimal"
           />
-          <CalculatorButton className="button" value="=" />
+          <CalculatorButton
+            className="button"
+            value="="
+            buttonClick={buttonClick}
+            type="equals"
+          />
         </div>
       </div>
     </div>
